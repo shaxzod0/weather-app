2126bc4dcfab72a7e097ecf2080ac647c2fa71e7
Shakhzod <azamatjonov90@gmail.com>
2022-02-23 09:03:14 +0500

    Initial Commit

---
+++ Weather app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather app
+//
+//  Created by Shaxzod Azamatjonov on 23/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
